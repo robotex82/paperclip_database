b0a520f3f0b66ab9232efea914715e1d6d5f6c97
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-10-12 11:19:49 +0200

    Update attachments_responder_middleware.rb
    Fixed model class name lookup for namespaced models.

--- lib/paperclip_database/attachments_responder_middleware.rb
+++ lib/paperclip_database/attachments_responder_middleware.rb
@@ -19,7 +19,9 @@ module PaperclipDatabase
     end
 
     def send_image(env)
-      id, attachment_name, klass_name = env['PATH_INFO'].split('/').reverse
+      id, attachment_name, *klass_name =  env['PATH_INFO'].split('/').reverse
+
+      klass_name = klass_name.reverse.reject!(&:blank?).join("_")
       style = Rack::Utils.parse_query(env['QUERY_STRING'], '&')['style']
 
       model = klass_name.classify.constantize.send(:find, id)
