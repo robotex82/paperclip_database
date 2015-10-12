b2d1e71d3f11b25e472091c3b03865db1ae97782
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-10-12 11:31:18 +0200

    Update attachments_responder_middleware.rb
    Fixed model name generation for namespaced and camel cased models.

--- lib/paperclip_database/attachments_responder_middleware.rb
+++ lib/paperclip_database/attachments_responder_middleware.rb
@@ -21,7 +21,7 @@ module PaperclipDatabase
     def send_image(env)
       id, attachment_name, *klass_name =  env['PATH_INFO'].split('/').reverse
 
-      klass_name = klass_name.reverse.reject!(&:blank?).join("_")
+      klass_name = klass_name.reverse.reject!(&:blank?).join('/').camelize
       style = Rack::Utils.parse_query(env['QUERY_STRING'], '&')['style']
 
       model = klass_name.classify.constantize.send(:find, id)
